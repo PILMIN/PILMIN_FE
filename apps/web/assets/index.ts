248da11ad78580c14cc04a6655d5b311fbfc6397
민수 <mandoo4137@gmail.com>
2025-12-30 08:19:49 +0900

    refactor : assets, layout

---
+++ apps/web/assets/index.ts
@@ -0,0 +1,6 @@
+export { default as chatImg } from "./chat.svg";
+export { default as electricImg } from "./electric.svg";
+export { default as graphImg } from "./graph.svg";
+export { default as guardImg } from "./guard.svg";
+export { default as peopleImg } from "./people.svg";
+export { default as voteImg } from "./vote.svg";
